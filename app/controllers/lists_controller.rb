fd36ae00d709656cc3f8f5295a35dd053d14b6c8
ate-9 <adam.t.eldridge@gmail.com>
2025-07-04 00:39:26 +0900

    リストの作成/削除まで

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,4 @@
-class ListController < ApplicationController
+class ListsController < ApplicationController
   before_action :set_list, only: %i[ show edit update destroy ]
   before_action :set_items, only: %i[ index show edit update destroy ]
 
@@ -18,22 +18,34 @@ class ListController < ApplicationController
 
   def create
     @list = List.new(list_params)
+    if @list.save
+      redirect_to @list, notice: 'リストを作成しました！🎉'
+    else
+      render :new
+    end
   end
   def update
+    if @list.update(list_params)
+      redirect_to @list, notice: 'リストを更新しました！🎉'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @list.destroy
+    redirect_to lists_path, notice: "リストを削除しました"
   end
 
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_list
-    @list = List.find(params.expect(:id))
+    @list = List.find(params[:id])
   end
 
   # Only allow a lists of trusted parameters through.
   def list_params
-    params.fetch(:list, {})
+    params.require(:list).permit(:list_name, :due_date, :author_id, :buyer_id, :store, :point_card, :memo, :draft, :complete)
   end
 
   def set_items
